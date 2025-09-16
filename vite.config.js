769df97313853a8408f9852ddf05059a2b08a289
Ruslana Onyshchuk <lalelya7@gmail.com>
2025-09-16 11:26:57 +0200

    update config for deployment

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  base: '/CoreAI_landing/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: { '@': path.resolve(__dirname, 'src') },
